f3d118989f6391c65e1bdccf8c935772b0f55575
Aleksandr Zaytsev <13rentgen@gmail.com>
2015-03-05 00:47:00 +0300

    Add example with video. Bug fix with MIME type text/plain in Chrome.

--- javascripts/main.js
+++ javascripts/main.js
@@ -86,3 +86,72 @@ var
     this.document,
     datGui.addFolder('Uploaded image controller')
 );
+
+/**
+ * Render test video
+ */
+!function( window, document, gui ) {
+    var
+        body      = document.getElementsByTagName('body')[0],
+        video     = document.getElementsByTagName('video')[0],
+        canvas    = document.getElementById('videoTestImageContainer'),
+        c         = canvas.getContext('2d'),
+
+        detailController,
+
+        renderToCanvas = function( result ) {
+            var
+                rows    = result.length,
+                cols    = result[0].length,
+
+                yOffset = 0, // Free space beetween symbols
+                xOffset = 0, // Free space beetween symbols
+
+                font, stepX, stepY, i, j, symbolInfo;
+            // end of vars
+
+            canvas.width  = video.width;
+            canvas.height = video.height;
+
+            stepX = video.width/cols;
+            stepY = video.height/rows;
+
+            font  = stepY + 1 + 'px monospace';
+
+            for ( i = 0; i < rows; i++ ) {
+                for ( j = 0; j < cols; j++ ) {
+                    symbolInfo  = result[i][j];
+                    c.fillStyle = 'rgb(' + symbolInfo.r +', ' + symbolInfo.g +', ' + symbolInfo.b +')';
+                    c.font      = font;
+
+                    c.fillText(symbolInfo.sym, j * (stepX + xOffset), i * (stepY + yOffset));
+                }
+            }
+        },
+
+        schwartz = new Schwartz({
+            render: renderToCanvas,
+            detail: 35
+        });
+    // end of vars
+
+    schwartz.generateFromVideo(video);
+
+    detailController  = gui.add(schwartz, 'detail', 1, 100);
+    inverseController = gui.add(schwartz, 'inverse');
+    gui.open();
+
+    detailController.onFinishChange(function( value ) {
+        schwartz.setDetail(value);
+        schwartz.generateFromVideo(video);
+    });
+
+    inverseController.onFinishChange(function( value ) {
+        schwartz.inverseImage();
+        schwartz.generateFromVideo(video);
+    });
+}(
+    this,
+    this.document,
+    datGui.addFolder('Video controller')
+);
